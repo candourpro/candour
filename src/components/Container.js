442f7269fb9a26fc320075b3972bc3556df049cd
Domas <domas.bitvinskas@me.com>
2018-08-06 12:08:07 +0300

    Refactor container/props/tag

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,67 +1,41 @@
-import React, { Component } from 'react';
-import { compose } from 'redux';
-import Radium from 'radium';
-import windowSize from 'react-window-size';
-import _ from 'lodash';
-import filterInvalidDOMProps from 'filter-invalid-dom-props';
+import React from 'react'
+import Radium from 'radium'
+import _ from 'lodash'
+import filterInvalidDOMProps from 'filter-invalid-dom-props'
 
-import step from '@candour/step';
-import fluid from '@candour/fluid';
-import maxReadableWidth from '../theme/maxReadableWidth';
 import isSmall from '../theme/isSmall';
 import { CandourConsumer } from '../index';
 import style from '../helpers/style'
 
-class Container extends Component {
-  isHtmlElement() {
-    return _.isString(this.component());
+const isHtmlElement = _.isString
+
+const childrenProps = (props, component) => {
+  if (!isHtmlElement(component)) {
+    return _.pick(props, _.keys(component.propTypes).concat('onMouseEnter', 'onMouseLeave'))
   }
 
-  tagName() {
-    return this.isHtmlElement() ? this.component() : Radium(this.component());
-  }
+  return filterInvalidDOMProps(props)
+}
 
-  component() {
-    const {
-      component = 'div',
-    } = this.props;
+export default Radium(({
+  children,
+  component = 'div',
+  candourName = 'container',
+  candourBase = 'base',
+  ...rest,
+}) => {
+  const Tag = component
 
-    return component;
-  }
-
-  childProps() {
-    if (!this.isHtmlElement()) {
-      return _.pick(this.props, _.keys(this.component().propTypes).concat('onMouseEnter', 'onMouseLeave'));
-    }
-
-    return filterInvalidDOMProps(this.props);
-  }
-
-  render() {
-    const {
-      children,
-      candourName = 'container',
-      candourBase = 'base',
-    } = this.props;
-
-    const TagName = this.tagName();
-
-    return (
-      <CandourConsumer>
-        {theme => (
-          <TagName
-            {...this.childProps()}
-            style={style(theme, this.props, candourName, candourBase)}
-          >
-            {children}
-          </TagName>
-        )}
-      </CandourConsumer>
-    );
-  }
-};
-
-export default compose(
-  windowSize,
-  Radium,
-)(Container);
+  return (
+    <CandourConsumer>
+      {theme => (
+        <Tag
+          {...childrenProps(rest, Tag)}
+          style={style(theme, rest, candourName, candourBase)}
+        >
+          {children}
+        </Tag>
+      )}
+    </CandourConsumer>
+  )
+})
