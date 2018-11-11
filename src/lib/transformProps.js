baacaf04dfeed45aa188b70aff1d77e3ffbb7700
Domas <domas.bitvinskas@me.com>
2018-11-11 14:49:00 +0000

    v5.0.3: fix shorthand false boolean transform

--- src/lib/transformProps.js
+++ src/lib/transformProps.js
@@ -3,13 +3,12 @@ import cssNameMatch from './cssNameMatch'
 import cssProperties from 'css-properties'
 import camelCaseCss from 'camelcase-css'
 
-const propertyAndValueMatch = (key) => {
+const shorthand = (key) => {
   if (cssNameMatch(key)) return
 
   let result
 
   _.eachRight(_.sortBy(cssProperties, 'length'), (prop) => {
-
     result = key.match(new RegExp(`^(${camelCaseCss(prop)})(.+)`))
     if (result) return false
   })
@@ -19,13 +18,15 @@ const propertyAndValueMatch = (key) => {
 
 export default (props) => (
   _.reduce(props, (memo, value, key) => {
-    const match = propertyAndValueMatch(key)
+    const match = shorthand(key)
 
     if (match) {
-      memo[match[1]] = _.kebabCase(match[2])
-
-      if (!_.isBoolean(value)) {
-        console.warn(`Candour: shorthand property used with a value at ${key}.`)
+      if (_.isBoolean(value)) {
+        if (value) {
+          memo[match[1]] = _.kebabCase(match[2])
+        }
+      } else {
+        console.warn(`Candour: shorthand property used with a non-boolean value at ${key}.`)
       }
     } else {
       memo[key] = value
