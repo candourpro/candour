83cca92d04b6283acb455efcdf68716d13cd3f30
Domas <domas.bitvinskas@me.com>
2019-03-05 13:51:40 +0200

    v5.5.1: Remove automagical Heading component setting by level

--- src/primitives/Heading.js
+++ src/primitives/Heading.js
@@ -3,12 +3,11 @@ import Container from './Container'
 import levelFromProps from '../lib/levelFromProps'
 
 export default ({
-  component: componentProp,
+  component = 'h1',
   primitiveType = 'heading',
   ...rest,
 }) => {
   const level = levelFromProps(rest) || 1
-  const component = componentProp || level > 6 ? 'div' : `h${level}`
 
   return (
     <Container
