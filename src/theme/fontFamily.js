6cf03a4683e9147e4edbe4ff41207c90b54da45e
Domas <domas.bitvinskas@me.com>
2019-03-31 13:58:43 +0300

    v5.7.0. Remove default fontFamily; improve React Native support

--- src/theme/fontFamily.js
+++
@@ -1,1 +0,0 @@
-export default `"SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import fontFamily from './fontFamily'
 import levels from '../lib/levels'
 
 const sizes = [4.5, 2.5, 2, 1.5, 1.25, 1, 0.85, 0.75, 0.65, 0.55]
@@ -13,9 +12,7 @@ export default (converters) => {
 
   return ({
     container: {
-      base: {
-        fontFamily,
-      },
+      base: {},
       ...levels('fontSize', _.map(sizes, convert)),
     },
     input: {
